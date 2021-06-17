f89991060e2f1a921f4fc9440059621f0a08a4a4
IMAN SIAM <ImanIssa1997@gmail.com>
2021-06-17 12:58:48 +0300

    Bitly

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UrlsController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/*Route::get('/', function () {
+    return view('welcome');
+});*/
+Route::get('/', [UrlsController::class,'index'])->name('index')->middleware('auth');
+
+Route::get('/create/{id}', [UrlsController::class ,'create'])->name('create');
+Route::post('/', [UrlsController::class, 'store'])->name('store');
+Route::get('/{code}', [UrlsController::class, 'show'])->name('show');
+//Route::get('/{code}', [UrlsController::class, 'show'])->name('show');
+
+//
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+require __DIR__.'/auth.php';
+
